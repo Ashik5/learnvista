6a5f84a3f8f5cba3563ac9b35f90b8003facc31f
Antikdhar <antikdhar1357@gmail.com>
2024-06-23 13:22:01 +0600

    admin

---
+++ src/views/admin/tables/index.jsx
@@ -0,0 +1,17 @@
+import CheckTable from "./components/CheckTable";
+
+import {
+  columnsDataCheck,
+} from "./variables/columnsData";
+import tableDataCheck from "./variables/tableDataCheck.json";
+const Tables = () => {
+  return (
+    <div>
+      <div className="mt-5 grid h-full">
+        <CheckTable columnsData={columnsDataCheck} tableData={tableDataCheck} />
+      </div>
+    </div>
+  );
+};
+
+export default Tables;
