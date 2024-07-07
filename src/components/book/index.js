c8c34701402ab91af39b7f468cc5c79b98efa995
Ashik Mahmud <ashikmamud123@gmail.com>
2024-07-07 13:16:31 +0600

    env variabel

---
+++ src/components/book/index.js
@@ -0,0 +1,23 @@
+import course from 'assets/svg/course.svg'
+export default function BookCard({Name,AuthorName,Price}){
+    return(
+        <div className='relative'>
+        <img src={course} alt="Course Cover" className="w-full h-auto" />
+        <div className="p-2">
+            <div className='flex flex-row justify-between'>
+                <p className="text-green-600 font-bold">{Name}</p>
+                <p className="text-yellow-400 ">★★★★☆</p>
+            </div>
+            <p className="font-medium ">Author : {AuthorName}</p>
+            <p className="text-gray-600 text-sm">Is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy</p>
+            <div className="flex justify-between items-center mb-6">
+                <p className='py-1 gap-3'>${Price}</p>
+                <p>1.2k+ Sales</p>
+            </div>
+            <div className="text-center">
+                <button className="bg-green-500 text-white border-none px-3 py-1 rounded-full cursor-pointer text-lg hover:bg-green-600">Buy Now</button>
+             </div> 
+        </div>
+    </div>
+    )
+}
