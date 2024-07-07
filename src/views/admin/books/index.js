a435163ff5a4d27782ab5efe0f6136a813939917
Ashik Mahmud <ashikmamud123@gmail.com>
2024-07-07 14:50:10 +0600

    add book

--- src/views/admin/books/index.js
+++ src/views/admin/books/index.js
@@ -33,9 +33,9 @@ export default function Books() {
     if (loading) {
         return <div class="flex justify-center items-center h-screen">
             <div class="relative inline-flex">
-                <div class="w-8 h-8 bg-blue-500 rounded-full"></div>
-                <div class="w-8 h-8 bg-blue-500 rounded-full absolute top-0 left-0 animate-ping"></div>
-                <div class="w-8 h-8 bg-blue-500 rounded-full absolute top-0 left-0 animate-pulse"></div>
+                <div class="w-8 h-8 bg-green-500 rounded-full"></div>
+                <div class="w-8 h-8 bg-green-500 rounded-full absolute top-0 left-0 animate-ping"></div>
+                <div class="w-8 h-8 bg-green-500 rounded-full absolute top-0 left-0 animate-pulse"></div>
             </div>
         </div>;
     }
