7e2aaf39dd145a9fb4cbc9731a141c7c964c5f34
Antikdhar <antikdhar1357@gmail.com>
2024-07-13 00:24:32 +0600

    checkout

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { Routes, Route, Navigate } from "react-router-dom";
 import AdminLayout from "layouts/admin";
 import AuthLayout from "layouts/auth";
 import Allbooks from "views/allbooks";
+import Checkout from "views/checkout";
 
 import Addbooks from "views/addbooks";
 
@@ -13,6 +14,7 @@ const App = () => {
       <Route path="addbooks" element={<Addbooks />} />
       <Route path="allbooks" element={<Allbooks />} />
       <Route path="addbooks" element={<AddBook />} />
+      <Route path="checkout" element={<Checkout />} />
       <Route path="auth/*" element={<AuthLayout />} />
       <Route path="admin/*" element={<AdminLayout />} />
       <Route path="/" element={<Navigate to="/admin" replace />} />
